fa039565aadbde5ef1e76ff75be7493cc4f79c80
shreyash0185 <shreyashmuktalkar98@gmail.com>
2025-06-22 17:26:49 +0530

    Intial commit

---
+++ src/main/java/com/smart_parking_lot/smart_parking_lot/SmartParkingLotApplication.java
@@ -0,0 +1,13 @@
+package com.smart_parking_lot.smart_parking_lot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingLotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartParkingLotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_parking_lot/smart_parking_lot/SmartParkingLotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_parking_lot.smart_parking_lot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingLotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
